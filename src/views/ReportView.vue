63a8ef85b10e91e9be06565af478dd0f9f48d150
Sergey Semenov <sergey.semenov@smartseeds.ru>
2023-02-27 14:13:30 +0300

    update ref

--- src/views/ReportView.vue
+++ src/views/ReportView.vue
@@ -23,19 +23,19 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, type Ref } from 'vue';
+
 import GetReportUseCase from '@/use-cases/GetReportUseCase/GetReportUseCase';
 import ReportDetailRepository from '@/infastructure/ReportDetailRepository/ReportDetailRepository';
 import UserFractionRepository from '@/infastructure/UserFractionRepository/UserFractionRepository';
-import type { ISummaryReport, IFractionSummaryReport } from '@/use-cases/GetReportUseCase/IGetReportResponseModel';
-
 import UserRepository from '@/infastructure/UserRepository/UserRepository';
+import type { ISummaryReport, IFractionSummaryReport } from '@/use-cases/GetReportUseCase/IGetReportResponseModel';
 
 import Summary from '@/components/Report/Summary.vue';
 import FractionSummary from '@/components/Report/FractionSummary.vue';
 
 const summary = ref<ISummaryReport>();
-const fractions = ref<IFractionSummaryReport[]>([]);
+const fractions: Ref<IFractionSummaryReport[]> = ref([]);
 
 function onChangeFile(event: Event) {
     const target = event.target as HTMLInputElement;
