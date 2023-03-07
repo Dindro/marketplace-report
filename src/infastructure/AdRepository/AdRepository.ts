73590d697146330f096b9d3f3d5a95ce2077442b
Sergey Semenov <sergey.semenov@smartseeds.ru>
2023-03-07 11:10:56 +0300

    add ads repository

---
+++ src/infastructure/AdRepository/AdRepository.ts
@@ -0,0 +1,15 @@
+export default class AdRepository {
+    private readonly adConfig;
+
+    constructor(adConfig) {
+        this.adConfig = adConfig;
+    }
+
+    getCostList() {
+
+    }
+
+    getPlacement() {
+        
+    }
+}
