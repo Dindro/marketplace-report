8099e66e14c4008b91cc632b89a9c394e07504e6
Sergey Semenov <sergey.semenov@smartseeds.ru>
2023-05-01 10:35:58 +0300

    add user fraction

--- src/components/Version/VersionHistory.vue
+++ src/components/Version/VersionHistory.vue
@@ -24,6 +24,13 @@
 
     const data: IVersion[] = [
         {
+            date: '01.05.2023',
+            time: '10:30',
+            points: [
+                'Добавил доли',
+            ],
+        },        
+        {
             date: '24.04.2023',
             time: '13:00',
             points: [
