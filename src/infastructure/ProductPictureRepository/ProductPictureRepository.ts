5112564047ec213a7e400e01c31224038e5efd76
Sergey Semenov <sergey.semenov@smartseeds.ru>
2024-04-11 08:58:32 +0300

    fix show picture

--- src/infastructure/ProductPictureRepository/ProductPictureRepository.ts
+++ src/infastructure/ProductPictureRepository/ProductPictureRepository.ts
@@ -5,16 +5,16 @@ export default class ProductPictureRepository implements IProductPictureReposito
     get(code: number): Promise<ProductPicture> {
         const preview = this.getPreview(code);
         const picture = new ProductPicture(preview);
-        
+
         return Promise.resolve(picture);
     }
 
     private getPreview(code: number): string {
         // Функционал позаимствован с wildberries/main.js
-        let oe = Math.floor(code / 1e5),
-            xe = oe >= 0 && oe <= 143 ? "//basket-01.wb.ru/" : oe >= 144 && oe <= 287 ? "//basket-02.wb.ru/" : oe >= 288 && oe <= 431 ? "//basket-03.wb.ru/" : oe >= 432 && oe <= 719 ? "//basket-04.wb.ru/" : oe >= 720 && oe <= 1007 ? "//basket-05.wb.ru/" : oe >= 1008 && oe <= 1061 ? "//basket-06.wb.ru/" : oe >= 1062 && oe <= 1115 ? "//basket-07.wb.ru/" : oe >= 1116 && oe <= 1169 ? "//basket-08.wb.ru/" : oe >= 1170 && oe <= 1313 ? "//basket-09.wb.ru/" : oe >= 1314 && oe <= 1601 ? "//basket-10.wb.ru/" : oe >= 1602 && oe <= 1655 ? "//basket-11.wb.ru/" : oe >= 1656 && oe <= 1919 ? "//basket-12.wb.ru/" : "//basket-13.wb.ru/",
-            de = `vol${Math.floor(code / 1e5)}/part${Math.floor(code / 1e3)}/${code}/images/c246x328/1.jpg`;
+        let p = Math.floor(code / 1e5),
+            w = Math.floor(code / 1e3);
 
-        return `https:${xe}${de}`;
+        const url = `//basket-${p >= 0 && p <= 143 ? "01" : p >= 144 && p <= 287 ? "02" : p >= 288 && p <= 431 ? "03" : p >= 432 && p <= 719 ? "04" : p >= 720 && p <= 1007 ? "05" : p >= 1008 && p <= 1061 ? "06" : p >= 1062 && p <= 1115 ? "07" : p >= 1116 && p <= 1169 ? "08" : p >= 1170 && p <= 1313 ? "09" : p >= 1314 && p <= 1601 ? "10" : p >= 1602 && p <= 1655 ? "11" : p >= 1656 && p <= 1919 ? "12" : p >= 1920 && p <= 2045 ? "13" : "14"}.wb.ru/vol${p}/part${w}/${code}/images/c246x328/1.jpg`
+        return `https:${url}`;
     }
 }
